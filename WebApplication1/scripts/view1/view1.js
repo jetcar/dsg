aa506dc022ba1969d07be46c184c5d40cac264eb
vladimir.kruglov <vladimir.kruglov@arvato.com>
2016-08-18 11:02:21 +0300

    include js files

--- WebApplication1/scripts/view1/view1.js
+++ WebApplication1/scripts/view1/view1.js
@@ -2,7 +2,7 @@
 angular.module('myApp.view1', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/view1', {
-        templateUrl: 'view1/view1.html',
+        templateUrl: 'scripts/view1/view1.html',
         controller: 'View1Ctrl'
     });
 }
