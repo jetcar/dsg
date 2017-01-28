9b2067adb89b5e5717200b17e8dcdfbec63c18b4
jetcar <jetcarq@gmail.com>
2017-01-28 11:42:31 +0200

    fix references, change port

--- node/server.js
+++ node/server.js
@@ -59,6 +59,6 @@ function errorHandler(err, req, res, next) {
     res.end(err.message);
 }
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+    console.log('Example app listening on port 80!');
 });
